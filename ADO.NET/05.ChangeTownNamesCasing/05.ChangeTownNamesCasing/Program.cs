b8a91229d5328d06cb5fd345d1aa672b5f50c3fe
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2021-02-16 23:49:33 +0200

    Add exercise

---
+++ ADO.NET/05.ChangeTownNamesCasing/05.ChangeTownNamesCasing/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace _05.ChangeTownNamesCasing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(@"Server=.\SQLEXPRESS; Database=MinionsDB; Integrated Security=true");
+            connection.Open();
+
+            string country = Console.ReadLine();
+
+            using (connection)
+            {
+                SqlCommand checkIfCountryExists = new SqlCommand(@"SELECT Id FROM Countries WHERE Name = @country", connection);
+                checkIfCountryExists.Parameters.AddWithValue("@country", country);
+
+                int? countryId = (int?)checkIfCountryExists.ExecuteScalar();
+
+                if (countryId == null)
+                {
+                    Console.WriteLine("No town names were affected.");
+                    return;
+                }
+
+                SqlCommand checkIfCountryHaveTowns = new SqlCommand($@"SELECT Name FROM Towns WHERE CountryCode = {countryId}", connection);
+
+                string countries = (string)checkIfCountryHaveTowns.ExecuteScalar();
+
+                if (countries == null)
+                {
+                    Console.WriteLine("No town names were affected.");
+                    return;
+                }
+
+                SqlCommand updateToUpper = new SqlCommand($@"UPDATE Towns SET Name = UPPER(Name)
+                                                             WHERE CountryCode = (SELECT Id FROM Countries WHERE Name = '{country}')", connection);
+
+                int countriesAffected = updateToUpper.ExecuteNonQuery();
+
+                Console.WriteLine($"{countriesAffected} town names were affected.");
+
+                SqlDataReader reader = checkIfCountryHaveTowns.ExecuteReader();
+
+                List<string> countryNames = new List<string>();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        countryNames.Add((string)reader["Name"]);
+                    }
+                }
+
+                Console.WriteLine($"[{string.Join(", ", countryNames)}]");
+            }
+        }
+    }
+}
---
+++ ADO.NET/06.RemoveVillain/06.RemoveVillain/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _06.RemoveVillain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(@"Server=.\SQLEXPRESS; Database = MinionsDB; Integrated Security=true");
+            connection.Open();
+
+            string inputId = Console.ReadLine();
+
+            using (connection)
+            {
+                SqlCommand checkIfVillainExists = new SqlCommand(@"SELECT * FROM Villains WHERE Id = @villainId", connection);
+                checkIfVillainExists.Parameters.AddWithValue("@villainId", inputId);
+
+                int? villainId = (int?)checkIfVillainExists.ExecuteScalar();
+
+                if (villainId == null)
+                {
+                    Console.WriteLine("No such villain was found.");
+                    return;
+                }
+
+                SqlCommand getVillinName = new SqlCommand($@"SELECT Name FROM Villains WHERE Id = '{inputId}'", connection);
+
+                string villainName = (string)getVillinName.ExecuteScalar();
+
+                SqlCommand deleteVillainMinions = new SqlCommand($@"DELETE FROM MinionsVillains WHERE VillainId = '{inputId}'", connection);
+
+                int countMinions = deleteVillainMinions.ExecuteNonQuery();
+
+                SqlCommand deleteVillain = new SqlCommand($@"DELETE FROM Villains WHERE Id = '{inputId}'", connection);
+
+                deleteVillain.ExecuteNonQuery();
+
+                Console.WriteLine($"{villainName} was deleted.");
+                Console.WriteLine($"{countMinions} minions were released.");
+            }
+        }
+    }
+}
